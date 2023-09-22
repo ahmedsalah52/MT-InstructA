79789dea7e515f1f4b73914b6227f09f9bd70e4f
Ahmed <ahmed_salah1996@yahoo.com>
2023-09-23 00:28:15 +0200

    door close init right pos

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_door_close_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_door_close_v2.py
@@ -18,7 +18,6 @@ class SawyerDoorCloseEnvV2(SawyerDoorEnvV2):
         
         goal_low  = (self.task_offsets_min[0]-.3,self.task_offsets_min[1] + 0.5, 0.1499)
         goal_high = (self.task_offsets_max[0]-.3,self.task_offsets_max[1] + 0.5, 0.1501)
-        min_x = self.task_offsets_min[0]
         max_x = self.task_offsets_max[0]
         self.hand_init_pos_  = [np.random.uniform(min(-0.1,-0.8) ,max(0.1,max_x)),np.random.uniform(0.4,0.7), np.random.uniform(0.15,0.3)]
 
