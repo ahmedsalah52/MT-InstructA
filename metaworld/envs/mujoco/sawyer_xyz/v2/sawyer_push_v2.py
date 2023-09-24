5d58da888159f6d436448bb9d203cd34ca456895
Ahmed <ahmed_salah1996@yahoo.com>
2023-09-24 17:52:19 +0200

    adding push task

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_push_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_push_v2.py
@@ -6,9 +6,8 @@ from metaworld.envs import reward_utils
 from metaworld.envs.asset_path_utils import full_v2_path_for
 from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _assert_task_is_set
 
-from metaworld.envs.build_random_envs import build_env , multi_object_man
+from metaworld.envs.build_random_envs import Multi_task_env
 import os
-import glob,random
 
 class SawyerPushEnvV2(SawyerXYZEnv):
     """
@@ -25,54 +24,21 @@ class SawyerPushEnvV2(SawyerXYZEnv):
     """
     TARGET_RADIUS=0.05
 
-    def __init__(self):
-        hand_low = (-0.5, 0.40, 0.05)
-        hand_high = (0.5, 1, 0.5)
+    def __init__(self,main_pos_index=None , task_variant = None):
+        Multi_task_env.__init__(self)
+        self.main_pos_index = main_pos_index
+        self.task_variant = task_variant
+
+        hand_low = (-0.6, 0.30, 0.05)
+        hand_high = (0.6, 1, 0.5)
         main_file = 'sawyer_push_v2.xml'
-        generate = False
-        if generate:
-            mjcfs_dir = 'metaworld/envs/assets_v2/sawyer_xyz_multi/mjcfs/'+main_file.split('.')[0]
-            if not os.path.isdir(mjcfs_dir):
-                os.system('mkdir '+mjcfs_dir)
-            multi_object = multi_object_man(init_file_name=main_file)
+        self.generate_env(main_file,main_pos_index,task_variant)
 
-            main_envs_dir = 'metaworld/envs/assets_v2/sawyer_xyz/'
-            xml_files = os.listdir(main_envs_dir)
-            poses_list = [0,1,2]
-            for pos in [0,1,2]:
-                poses_list = [0,1,2]
-                dx_idx = poses_list.pop(pos)
-                for st_sec_file in xml_files:
-                    if main_file == st_sec_file: pass
-                    for nd_sec_file in xml_files:
-                        if nd_sec_file == st_sec_file or nd_sec_file == main_file: pass  
-                        try:
-                            multi_object.get_new_env([st_sec_file,nd_sec_file] , dx_idx,poses_list)
-                            self.file_name = multi_object.get_file_name()
-                            super().__init__(
-                                self.model_name,
-                                hand_low=hand_low,
-                                hand_high=hand_high,
-                            )
-                            multi_object.multi_env_loaded()
-                            
-                        except:
-                            print('failed to load:',self.file_name)
-                            multi_object.multi_env_not_loaded()
 
-        else:
-            env_txt_file = open('metaworld/all_envs/'+main_file.split('.')[0]+'.txt','r')
-            env_txt_lines = env_txt_file.read().split('\n')
-            
-            env_txt_line = random.choice(env_txt_lines)
-            
-            self.file_name = env_txt_line
-            main_env_pos = float(self.file_name.split(',')[1])        
-
-        obj_low   = (main_env_pos, 0.6, 0.02)
-        obj_high  = (main_env_pos, 0.7, 0.02)
-        goal_low  = (main_env_pos-0.05, 0.8, 0.01)
-        goal_high = (main_env_pos+0.05, 0.9, 0.02)
+        obj_low   = (self.task_offsets_min[0]   ,self.task_offsets_min[1] + 0.65, 0.02)
+        obj_high  = (self.task_offsets_max[0]   ,self.task_offsets_max[1] + 0.65, 0.02)
+        goal_low  = (self.task_offsets_min[0]   ,self.task_offsets_min[1] + 0.8, 0.01)
+        goal_high = (self.task_offsets_max[0]   ,self.task_offsets_max[1] + 0.8, 0.02)
 
         super().__init__(
             self.model_name,
@@ -83,7 +49,7 @@ class SawyerPushEnvV2(SawyerXYZEnv):
         self.init_config = {
             'obj_init_angle': .3,
             'obj_init_pos': np.array([0., 0.6, 0.02]),
-            'hand_init_pos': np.array([0., 0.6, 0.2]),
+            'hand_init_pos': np.array(self.hand_init_pos_, dtype=np.float32),
         }
 
         self.goal = np.array([0.1, 0.8, 0.02])
