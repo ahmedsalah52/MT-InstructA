b68d30ff53899202185e02eee42f333e2183e97c
Ahmed <ahmed_salah1996@yahoo.com>
2023-10-04 01:35:24 +0200

    separate data generation from model

--- train_utils/model.py
+++ train_utils/model.py
@@ -11,6 +11,7 @@ import random
 import clip
 from torchvision import transforms
 from PIL import Image 
+import warnings
 
 
 
@@ -257,9 +258,13 @@ class base_model(pl.LightningModule):
                     instruction = random.choice(self.tasks_commands[task])
                     rendered_seq = []
                     while 1:
+                        if obs == None:
+                            #warning 
+                            warnings.warn("None Obs")
+                            continue
                         step_input = {'instruction':instruction}
                         images = [self.model.preprocess_image(Image.fromarray(np.uint8(img))) for img in info['images']]
-                        step_input['images']   = torch.stack(images).unsqueeze(0).to(self.device)
+                        step_input['images']   = torch.stack(images).unsqueeze(0).to(self.device)   
                         step_input['hand_pos'] = torch.tensor(np.concatenate((obs[0:4],obs[18:22]),axis =0)).to(torch.float32).unsqueeze(0).to(self.device)
                         a = self.model(step_input)
                         obs, reward, done,success, info = env.step(a.detach().cpu().numpy()[0]) 
