ee99f9d89c122d9518cb2c7d26412f4d08159207
Ahmed <ahmed_salah1996@yahoo.com>
2023-10-09 00:31:23 +0200

    dataset dir

--- generat_data.py
+++ generat_data.py
@@ -10,11 +10,11 @@ args = parser.parse_args()
 
 
 
-generator = Generate_data(meta_env,os.path.join(args.project_dir,args.data_dir),args.agents_dir,args.tasks,args.train_data_total_steps,args.agents_dict_dir,args.agent_levels)
+generator = Generate_data(meta_env,os.path.join(args.project_dir,args.data_dir,'data'),args.agents_dir,args.tasks,args.train_data_total_steps,args.agents_dict_dir,args.agent_levels)
 data_dict  = generator.generate_data()
 
 
-with open(os.path.join(args.project_dir,args.dataset_dict_dir), 'w') as f:
+with open(os.path.join(args.project_dir,args.data_dir,'dataset_dict.json'), 'w') as f:
     json.dump(data_dict, f)
 
 
--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def main():
     tasks_commands = json.load(open(args.tasks_commands_dir))
     model = base_model(args=args,tasks_commands=tasks_commands,env=meta_env,wandb_logger=wandb_logger,seed=args.seed)
 
-    train_dataset = MW_dataset(model.preprocess,os.path.join(args.project_dir,args.dataset_dict_dir),os.path.join(args.project_dir,args.data_dir),tasks_commands,total_data_len=args.train_data_total_steps)
+    train_dataset = MW_dataset(model.preprocess,os.path.join(args.project_dir,args.data_dir,'dataset_dict.json'),os.path.join(args.project_dir,args.data_dir,'data'),tasks_commands,total_data_len=args.train_data_total_steps)
     stats_table = train_dataset.get_stats()
     wandb_logger.log_table(key=f"Dataset Success Rate",  columns=['Task name','Success Rate'],data=stats_table)
 
