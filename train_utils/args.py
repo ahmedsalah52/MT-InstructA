3a0c152f1c5f5d9e563d0a8d992954e36ba83e4a
Ahmed <ahmed_salah1996@yahoo.com>
2023-10-18 19:00:48 +0200

    fix tasks list

--- train_utils/args.py
+++ train_utils/args.py
@@ -21,7 +21,7 @@ parser.add_argument('--with_imgs', type=bool, default=True,help='to render and s
 #shared args
 parser.add_argument('--data_dir', type=str, default='generated_data')
 #parser.add_argument('--dataset_dict_dir', type=str, default='generated_data/')
-parser.add_argument('--tasks', type=list, default= ['soccer-v2']) #['button-press-topdown-v2', 'button-press-v2', 'door-lock-v2', 'door-open-v2',  'drawer-open-v2', 'window-open-v2', 'faucet-open-v2', 'faucet-close-v2', 'handle-press-v2', 'coffee-button-v2'])
+parser.add_argument('--tasks', type=list, default= ['button-press-topdown-v2', 'button-press-v2', 'door-lock-v2', 'door-open-v2',  'drawer-open-v2', 'window-open-v2', 'faucet-open-v2', 'faucet-close-v2', 'handle-press-v2', 'coffee-button-v2'])
 parser.add_argument('--agents_dict_dir', type=str, default='training_configs/agents_dict.json')
 
 
