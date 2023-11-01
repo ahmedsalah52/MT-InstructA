c1f3fd6e503dfcbb0f0c73d7b4d38c7688a6be14
Ahmed <ahmed_salah1996@yahoo.com>
2023-11-01 10:17:04 +0100

    necks configs

--- train_utils/args.py
+++ train_utils/args.py
@@ -84,12 +84,12 @@ parser.add_argument('--op_image_model_name', type=str, default='ViT-B/32',choice
  'ViT-L/14',
  'ViT-L/14@336px'])
 
-#neck params
-parser.add_argument('--n_heads'     , type=int, default=8)
-parser.add_argument('--att_head_emp', type=int, default=128)
-parser.add_argument('--neck_layers' , type=int, default=1)
+#neck params  --n_heads 16 --att_head_emp 16 --neck_layers 2 --neck_max_len 200
+parser.add_argument('--n_heads'     , type=int, default=16)
+parser.add_argument('--att_head_emp', type=int, default=16)
+parser.add_argument('--neck_layers' , type=int, default=2)
 parser.add_argument('--neck_dropout', type=int, default=0.2)
-parser.add_argument('--neck_max_len', type=int, default=26)
+parser.add_argument('--neck_max_len', type=int, default=200)
 
 
 #head params
