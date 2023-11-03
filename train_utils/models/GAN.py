90869b6262c8ac3401cfd5ac2246d075cfb6d2e8
Ahmed <ahmed_salah1996@yahoo.com>
2023-11-03 17:08:11 +0100

    GAN progress

--- train_utils/models/GAN.py
+++ train_utils/models/GAN.py
@@ -2,6 +2,7 @@ from train_utils.models.base import arch
 
 import torch.nn as nn
 import torch
+from train_utils.models.seq import seq_model
 class Generator(arch):
     def __init__(self,args) -> None:
         super().__init__(args)
@@ -56,33 +57,51 @@ class Discriminator(arch):
             params += self.neck.get_opt_params()
         return params
 
-class simple_GAN(nn.Module):
+class simple_GAN(seq_model):
     def __init__(self,args) -> None:
-        super().__init__()
-        self.args = args
-        self.generator = Generator(args)
+        super().__init__(args)
+        self.noise_len = args.noise_len
         self.discriminator = Discriminator(args)
-        self.preprocess_image = self.generator.preprocess_image
-   
-    def forward(self,batch):
-        return self.generator(batch)
+    def encode(self,batch):
+        embeddings = []
+        for i in range(self.args.seq_len):
+            batch_step = {}
+            for k,vs in batch.items():
+                batch_step[k] = vs[i]
+           
+            batch_step = {k : v.to(self.dummy_param.device) if k != 'instruction' else v  for k,v in batch_step.items()}
+            x = self.backbone(batch_step)  
+            del batch_step
+        
+            self.normalize(x)
+            if self.args.neck:
+                x = self.neck(x)
+            embeddings.append(x)
+        return torch.stack(embeddings,dim=0)
     
-    def generator_step(self,batch):
-        real = torch.ones(batch['action'].size(0), 1)
-        real = real.type_as(batch['action'])
 
-        return self.discriminator.loss_fun(self.discriminator(batch,self(batch)),real)
+    def forward(self,embeddings):
+        xs , h = self.seq_module(embeddings)
+
+        outs = [self.head(x) for x in xs]
+        outs = torch.stack(outs,dim=0)
+        return outs
+    def generator_step(self,embeddings,actions):
+        real = torch.ones(actions.size(0), 1)
+        real = real.type_as(actions)
+
+        return self.discriminator.loss_fun(self.discriminator(embeddings,self(embeddings)),real)
     
-    def discriminator_step(self,batch):
+    def discriminator_step(self,embeddings,actions,generated_embeddings,generated_actions):
        #real
-        real = torch.ones(batch['action'].size(0), 1)
-        real = real.type_as(batch['action'])
-        real_loss = self.discriminator.loss_fun(self.discriminator(batch,batch['action']), real)
+        real = torch.ones(actions.size(0), 1)
+        real = real.type_as(actions)
+        real_loss = self.discriminator.loss_fun(self.discriminator(embeddings,actions), real)
 
         #fake
-        fake = torch.zeros(batch['action'].size(0), 1)
-        fake = fake.type_as(batch['action'])
-        fake_loss = self.discriminator.loss_fun(self.discriminator(batch,self(batch).detach()), fake)
+        fake = torch.zeros(generated_actions.size(0), 1)
+        fake = fake.type_as(generated_actions)
+        fake_loss = self.discriminator.loss_fun(self.discriminator(generated_embeddings,generated_actions.detach()), fake)
 
         return (real_loss + fake_loss) / 2
     
@@ -93,4 +112,3 @@ class simple_GAN(nn.Module):
         return [torch.optim.Adam(generator_params,lr=self.args.lr),
                 torch.optim.Adam(discriminator_params,lr=self.args.lr)]
 
-   
