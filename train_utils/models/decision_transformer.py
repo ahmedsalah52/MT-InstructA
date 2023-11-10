1e59c7e9a7dfbe4b4f2ec3c492272e97bf12b921
Ahmed <ahmed_salah1996@yahoo.com>
2023-11-10 04:04:23 +0100

    command one time per seq

--- train_utils/models/decision_transformer.py
+++ train_utils/models/decision_transformer.py
@@ -13,7 +13,9 @@ from train_utils.heads import *
 from train_utils.models.base import arch
 from collections import deque
 
-class DecisionTransformer(TrajectoryModel):
+
+
+class DecisionTransformer_(TrajectoryModel):
 
     """
     This model uses GPT to model (Return_1, state_1, action_1, Return_2, state_2, ...)
@@ -107,6 +109,115 @@ class DecisionTransformer(TrajectoryModel):
         # reshape x so that the second dimension corresponds to the original
         # returns (0) pos (1), command (2), state (3) , action (4); i.e. x[:,1,t] is the token for s_t
         x = x.reshape(batch_size, seq_length, self.step_len, self.hidden_size).permute(0, 2, 1, 3)
+
+        # get predictions
+        #command_preds = self.command_preds(x[:,0])  
+        #state_preds  = self.predict_state(x[:,2])    
+        action_preds = self.predict_action(x[:,-1])   # predict next action given state
+
+        return action_preds
+
+    
+
+class DecisionTransformer(TrajectoryModel):
+
+    """
+    This model uses GPT to model (Return_1, state_1, action_1, Return_2, state_2, ...)
+    """
+
+    def __init__(
+            self,
+            state_dim,
+            state_len,
+            act_dim,
+            command_dim,
+            pos_emp,
+            hidden_size,
+            max_length=None,
+            max_ep_len=4096,
+            action_tanh=True,
+            **kwargs
+    ):
+        super().__init__(state_dim, act_dim, max_length=max_length)
+
+        
+        config = transformers.GPT2Config(
+            vocab_size=1,  # doesn't matter -- we don't use the vocab
+            n_embd=hidden_size,
+            **kwargs
+        )
+        self.hidden_size = hidden_size
+        self.pos_emp = pos_emp
+        self.command_dim = command_dim
+        self.step_len = state_len + 2
+        # note: the only difference between this GPT2Model and the default Huggingface version
+        # is that the positional embeddings are removed (since we'll add those ourselves)
+        self.transformer = GPT2Model(config)
+
+        self.embed_timestep = nn.Embedding(max_ep_len, hidden_size)
+        self.embed_command = torch.nn.Linear(self.command_dim, hidden_size)
+        self.embed_state = nn.ModuleList([torch.nn.Linear(self.state_dim, hidden_size) for _ in range(state_len)]) 
+        self.embed_action = torch.nn.Linear(self.act_dim, hidden_size)
+        self.embed_pos = torch.nn.Linear(self.pos_emp, hidden_size)
+        self.embed_return = torch.nn.Linear(1, hidden_size)
+
+        self.embed_ln = nn.LayerNorm(hidden_size)
+
+        # note: we don't predict states or returns for the paper
+        #self.predict_state = torch.nn.Linear(hidden_size, self.state_dim)
+        self.predict_action = nn.Sequential(
+            *([nn.Linear(hidden_size, self.act_dim)] + ([nn.Tanh()] if action_tanh else []))
+        )
+        #self.command_preds = torch.nn.Linear(hidden_size, command_dim)
+
+    def forward(self, states, actions, poses, command,returns_to_go, timesteps, attention_mask=None):
+
+        batch_size, seq_length = states.shape[0], states.shape[1]
+        if attention_mask is None:
+            # attention mask for GPT: 1 if can be attended to, 0 if not
+            attention_mask = torch.ones((batch_size, seq_length), dtype=torch.long)
+
+        # embed each modality with a different head
+        state_embeddings = [state_emp(states[:,:,i,:]) for i,state_emp in enumerate(self.embed_state)]
+        #action_embeddings = self.embed_action(actions)
+        command_embeddings = self.embed_command(command)
+        time_embeddings = self.embed_timestep(timesteps)
+        pos_embeddings  = self.embed_pos(poses)
+        returns_embeddings  = self.embed_return(returns_to_go)
+
+        # time embeddings are treated similar to positional embeddings
+        state_embeddings = [state + time_embeddings for state in state_embeddings]
+        #action_embeddings = action_embeddings + time_embeddings
+        #command_embeddings = command_embeddings + time_embeddings
+        pos_embeddings  = pos_embeddings + time_embeddings
+        returns_embeddings = returns_embeddings + time_embeddings
+        # this makes the sequence look like (R_1, s_1, a_1, R_2, s_2, a_2, ...)
+        # which works nice in an autoregressive sense since states predict actions
+        stacked_inputs = torch.stack(
+            (returns_embeddings,pos_embeddings, *state_embeddings), dim=1
+        ).permute(0, 2, 1, 3).reshape(batch_size, self.step_len*seq_length, self.hidden_size)
+        stacked_inputs = self.embed_ln(stacked_inputs)
+
+        # to make the attention mask fit the stacked inputs, have to stack it as well
+        stacked_attention_mask = torch.stack(
+            [attention_mask]*self.step_len, dim=1
+        ).permute(0, 2, 1).reshape(batch_size, self.step_len*seq_length)
+
+        # we feed in the input embeddings (not word indices as in NLP) to the model
+        
+        #add command 
+        stacked_inputs = torch.cat((command_embeddings,stacked_inputs), dim=1)
+        stacked_attention_mask = torch.cat((stacked_attention_mask, torch.ones((batch_size, 1), dtype=torch.long).to(stacked_attention_mask.device)), dim=1)
+
+        transformer_outputs = self.transformer(
+            inputs_embeds=stacked_inputs,
+            attention_mask=stacked_attention_mask,
+        )
+        x = transformer_outputs['last_hidden_state']
+
+        # reshape x so that the second dimension corresponds to the original
+        # returns (0) pos (1), command (2), state (3) , action (4); i.e. x[:,1,t] is the token for s_t
+        x = x[:,1:].reshape(batch_size, seq_length, self.step_len, self.hidden_size).permute(0, 2, 1, 3)
 
         # get predictions
         #command_preds = self.command_preds(x[:,0])  
@@ -265,8 +376,7 @@ class DL_model(arch):
             states_embeddings.append(states)
             poses_embeddings.append(poses)
 
-            commands_embeddings.append(commands)
-
+        commands_embeddings.append(commands)
         
 
         states_embeddings   = torch.stack(states_embeddings,dim=0).transpose(1,0).to(self.dummy_param.device)
