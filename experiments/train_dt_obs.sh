f9f5876c4c4d56661bc80879f4f7cb16dba30ee1
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2023-12-21 18:45:34 +0100

    configs

--- experiments/train_dt_obs.sh
+++ experiments/train_dt_obs.sh
@@ -12,9 +12,8 @@ python train.py \
 --batch_size 64 \
 --dt_n_layer 8 \
 --dt_n_head 16 \
---dt_embed_dim 128 \
---run_name window_open \
---tasks window-open-v2 \
+--dt_embed_dim 1024 \
+--run_name dt_obs \
 --lr 1e-5 \
 --load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_neck2_2cams/checkpoints/epoch=10-train_loss=0.00.ckpt' \
 --freeze_modules 'backbone,neck'
