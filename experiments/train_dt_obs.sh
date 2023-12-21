54c71598bbae4b09a87156bbd991a311ba52ad99
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2023-12-21 16:27:46 +0100

    configs

--- experiments/train_dt_obs.sh
+++ experiments/train_dt_obs.sh
@@ -8,7 +8,7 @@ python train.py \
 --num_epochs 200000 \
 --checkpoint_every 500 \
 --opt_patience 1000 \
---evaluation_episodes 10 \
+--evaluation_episodes 3 \
 --weight_decay 1e-4 \
 --batch_size 64 \
 --dt_n_layer 8 \
@@ -16,4 +16,4 @@ python train.py \
 --dt_embed_dim 128 \
 --run_name window_open \
 --tasks window-open-v2 \
---lr 1e-4
+--lr 1e-5
