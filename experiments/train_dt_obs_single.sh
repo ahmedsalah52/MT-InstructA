dff27567b0e048546ba4ad933e34c897c6c07b16
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2023-12-22 13:56:19 +0100

    configs

--- experiments/train_dt_obs_single.sh
+++ experiments/train_dt_obs_single.sh
@@ -3,7 +3,7 @@
 python train.py \
 --model dt_obs \
 --dataset generated_data_multi_lvls \
---seq_len 5 \
+--seq_len 20 \
 --num_epochs 200000 \
 --checkpoint_every 500 \
 --opt_patience 1000 \
@@ -13,7 +13,7 @@ python train.py \
 --dt_n_layer 3 \
 --dt_n_head 1 \
 --dt_embed_dim 128 \
---run_name window_open \
+--run_name window_open_20 \
 --tasks window-open-v2 \
 --lr 1e-4  \
 --freeze_modules 'backbone,neck' \
