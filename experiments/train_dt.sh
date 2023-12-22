ef296b15d10d49f373f87c0607f372b8249f8655
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2023-12-22 01:24:37 +0100

    configs

---
+++ experiments/train_dt.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+python train.py \
+--model dt_obs \
+--dataset generated_data_multi_lvls \
+--seq_len 5 \
+--num_epochs 100 \
+--checkpoint_every 5 \
+--opt_patience 30 \
+--evaluation_episodes 10 \
+--weight_decay 1e-4 \
+--batch_size 64 \
+--dt_n_layer 5 \
+--dt_n_head 16 \
+--dt_embed_dim 1024 \
+--run_name dt \
+--lr 1e-4 \
+--load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_neck2_2cams/checkpoints/epoch=10-train_loss=0.00.ckpt' \
+--freeze_modules 'backbone,neck'
