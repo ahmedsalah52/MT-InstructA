143567ab06bdf9c62c0c5e015c1ca8c4d6fef7c7
Ahmed Mansour <am@Ahmeds-Laptop.local>
2024-01-11 00:13:55 +0100

    dt best film

--- experiments/train_dt.sh
+++ experiments/train_dt.sh
@@ -6,7 +6,7 @@ python train.py \
 --seq_len 5 \
 --cams '2,4' \
 --neck film \
---num_epochs 1000 \
+--num_epochs 100 \
 --checkpoint_every 1 \
 --opt_patience 30 \
 --evaluation_episodes 10 \
@@ -15,7 +15,7 @@ python train.py \
 --dt_n_layer 5 \
 --dt_n_head 16 \
 --dt_embed_dim 1024 \
---run_name dt \
+--run_name dt_best_film \
 --lr 1e-4 \
---load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/dt/checkpoints/epoch=29-success_rate=0.22.ckpt' \
+--load_checkpoint_path '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_neck2_2cams/checkpoints/epoch=17-train_loss=0.01.ckpt' \
 --freeze_modules 'backbone,neck'
