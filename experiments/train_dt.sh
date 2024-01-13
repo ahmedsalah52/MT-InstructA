2dcc8dd4f43fdcd6a3137643ad8064db1b12e5e8
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-01-13 12:45:14 +0100

    configs all model

--- experiments/train_dt.sh
+++ experiments/train_dt.sh
@@ -11,12 +11,12 @@ python train.py \
 --opt_patience 30 \
 --evaluation_episodes 10 \
 --weight_decay 1e-4 \
---batch_size 64 \
+--batch_size 16 \
 --dt_n_layer 5 \
 --dt_n_head 16 \
 --dt_embed_dim 1024 \
---run_name dt_split \
---run_notes 'cams splitted and predicted reward is used' \
+--run_name dt_all \
+--run_notes 'cams splitted train all model' \
 --lr 1e-4 \
 --load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_neck2_2cams/checkpoints/epoch=17-train_loss=0.01.ckpt' \
---freeze_modules 'backbone,neck'
+--use_env_reward \
