a7ff9342238f822781c01f8164a53d5c5424a4c4
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-01-14 21:28:53 +0100

    scripts

---
+++ experiments/train_film.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+python train.py \
+--model base \
+--neck film \
+--dataset generated_data_multi_lvls \
+--cams '2,4' \
+--num_epochs 100 \
+--checkpoint_every 1 \
+--opt_patience 5 \
+--evaluation_episodes 10 \
+--batch_size 32 \
+--run_name base_2cams \
+--run_notes 'base model 2 cams' \
+--lr 1e-4 \
+--use_env_reward \
+--n_gpus 2 \
