ba79f27af8d63f0b0b3c8dd522ca4369d20b2583
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-01-15 22:45:08 +0100

    cross att sh file

---
+++ experiments/eval_cross.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+echo "$1 -seed: $2"
+python evaluate_model.py \
+--model base \
+--dataset generated_data_multi_lvls \
+--cams '2,4' \
+--neck cross_attention \
+--evaluation_episodes 10 \
+--run_name "$1 -seed:$2" \
+--load_checkpoint_path "$1" \
+--seed $2
