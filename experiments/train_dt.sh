5cb0dcd3bf006734c8a2da03726761287cb0d2a0
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-02-02 00:43:49 +0100

    dt train script

--- experiments/train_dt.sh
+++ experiments/train_dt.sh
@@ -9,15 +9,16 @@ python train.py \
 --num_epochs 100 \
 --checkpoint_every 1 \
 --opt_patience 30 \
---evaluation_episodes 5 \
+--evaluation_episodes 10 \
 --weight_decay 1e-4 \
---batch_size 8 \
+--batch_size 64 \
 --dt_n_layer 5 \
 --dt_n_head 16 \
 --dt_embed_dim 1024 \
---run_name dt_all \
---run_notes 'cams splitted train all model' \
+--run_name dt \
+--run_notes 'no command for the gpt' \
 --lr 1e-4 \
 --load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_neck2_2cams/checkpoints/epoch=17-train_loss=0.01.ckpt' \
+--freeze_modules 'backbone,neck' \
 --use_env_reward \
---n_gpus 2 \
+--n_gpus 1 \
