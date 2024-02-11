21c6dec5613e8d75294328ea4e6d47742156b059
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-02-11 15:31:07 +0100

    configs

--- experiments/train_cross.sh
+++ experiments/train_cross.sh
@@ -11,10 +11,11 @@ python train.py \
 --num_epochs 100 \
 --checkpoint_every 1 \
 --opt_patience 5 \
---evaluation_episodes 3 \
+--evaluation_episodes 5 \
 --batch_size 32 \
 --run_name cross_2cams \
 --run_notes 'cross attention neck base model 2 cams' \
 --lr 1e-4 \
 --use_env_reward \
---n_gpus 4 \
+--n_gpus 2 \
+--head_use_instruction
