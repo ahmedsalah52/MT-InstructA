0b764308deeddecd9c9c2bfd52d1341e52c0bbee
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-02-15 13:19:26 +0100

    debugging

--- train_utils/metaworld_dataset.py
+++ train_utils/metaworld_dataset.py
@@ -231,8 +231,6 @@ class MW_dataset(Dataset):
             ret['instruction']  = random.choice(self.tasks_commands[task_name])
         else:
             ret['instruction']  = "do a stupid thing with "+task_name.replace('-v2','').replace('-',' ')
-            print('failed task:',task_name)       
-        print(ret['instruction'])
         return ret
     def prepare_padding_step(self,step_data):
         ret = {}
