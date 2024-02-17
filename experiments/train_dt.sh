e3a7bb504fa1456e363472e1857fbe14b54e5275
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-02-17 22:19:24 +0100

    configs

--- experiments/train_dt.sh
+++ experiments/train_dt.sh
@@ -14,9 +14,9 @@ python train.py \
 --batch_size 64 \
 --dt_n_layer 5 \
 --dt_n_head 16 \
---dt_embed_dim 512 \
---run_name dt_film_no_command_5l_512 \
---run_notes 'no command for the film and gpt with 5 layers aand 512 embed dim' \
+--dt_embed_dim 1024 \
+--run_name dt_film_no_command_5l_1024 \
+--run_notes 'no command for the film and gpt with 5 layers aand 1024 embed dim' \
 --lr 1e-4 \
 --load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_2cams_no_head_instruct/checkpoints/epoch=24-success_rate=0.89.ckpt' \
 --freeze_modules 'backbone,neck' \
