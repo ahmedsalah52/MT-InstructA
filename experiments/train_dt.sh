07444bf026f5b2581d310cd2e4555e492942f8e5
ahmedsalah52 <ahmed_salah1996@yahoo.com>
2024-02-18 23:35:53 +0100

    dt configs

--- experiments/train_dt.sh
+++ experiments/train_dt.sh
@@ -6,17 +6,17 @@ python train.py \
 --seq_len 5 \
 --cams '2,4' \
 --neck film \
---num_epochs 100 \
+--num_epochs 20 \
 --checkpoint_every 1 \
 --opt_patience 30 \
 --evaluation_episodes 10 \
 --weight_decay 1e-4 \
 --batch_size 64 \
---dt_n_layer 5 \
+--dt_n_layer 3 \
 --dt_n_head 16 \
---dt_embed_dim 1024 \
---run_name dt_film_no_command_5l_1024 \
---run_notes 'no command for the film and gpt with 5 layers aand 1024 embed dim' \
+--dt_embed_dim 128 \
+--run_name dt_film_N_prompt \
+--run_notes 'dt based on film with negative prompting with rtg' \
 --lr 1e-4 \
 --load_weights '/system/user/publicdata/mansour_datasets/metaworld/general_model/film_2cams_no_head_instruct/checkpoints/epoch=24-success_rate=0.89.ckpt' \
 --freeze_modules 'backbone,neck' \
