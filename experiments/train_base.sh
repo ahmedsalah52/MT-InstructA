586833e8be28c016092ff20cd60935325fcdff72
ahmed_salah1996@yahoo.com <ahmed_salah1996@yahoo.com>
2024-10-09 11:17:06 +0200

    configs base bc

--- experiments/train_base.sh
+++ experiments/train_base.sh
@@ -4,13 +4,13 @@ python train.py \
 --model base \
 --dataset generated_data_multi_lvls \
 --cams '2,4' \
---num_epochs 100 \
+--num_epochs 20 \
 --checkpoint_every 1 \
 --opt_patience 5 \
 --evaluation_episodes 10 \
 --batch_size 32 \
---run_name base_2cams \
+--run_name base_2cams_repeat \
 --run_notes 'base model 2 cams' \
 --lr 1e-4 \
 --use_env_reward \
---n_gpus 2 \
+--n_gpus 1 \
